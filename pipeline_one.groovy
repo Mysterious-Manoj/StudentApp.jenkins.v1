421f30b2ec6fe3108cbd65eb15ee05c78dea3fd4
manoj <manoj.com>
2024-05-26 12:12:18 +0530

    done

--- pipeline_one.groovy
+++ pipeline_one.groovy
@@ -3,7 +3,7 @@ pipeline {
       
       stages {
         stage('Pull') {
-            step {
+            steps {
                 git branch: 'main', changelog: false, poll: false, url: 'https://github.com/Mysterious-Manoj/maven-web-application.git'
                 echo 'here we pulling the repo'
             }
