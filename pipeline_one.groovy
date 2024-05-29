a1468ea030acbb34d019ab65a84b905d99010322
manoj <manoj.com>
2024-05-29 12:56:56 +0530

    done

--- pipeline_one.groovy
+++ pipeline_one.groovy
@@ -26,7 +26,7 @@ pipeline {
         stage('Deploy') {
             steps {
                 deploy adapters: [tomcat8(credentialsId: 'linux', path: '', url: 'http://13.40.165.187:8080/')], contextPath: '/', war: '**/*.war'
-                echo 'here we are Deploying the application.'
+                echo 'here we are Deploying the application'
             }
         }
     }
