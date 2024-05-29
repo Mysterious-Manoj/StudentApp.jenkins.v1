4ad6c7e3378c658b0dea6da9576845df4f8c9e59
manoj <manoj.com>
2024-05-29 11:49:12 +0530

    done

--- pipeline_one.groovy
+++ pipeline_one.groovy
@@ -11,9 +11,17 @@ pipeline {
          stage('Build') {
             steps {
                 sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
-                echo 'here we are building the repo'
+                echo 'here we are building the code'
             }
         }
-       
+       stage('Test') {
+            steps {
+               sh 'mvn sonar:sonar \
+                   -Dsonar.projectKey=student-ui \
+                   -Dsonar.host.url=http://18.169.133.138:9000 \
+                   -Dsonar.login=8262e22bbd953f784fddc566e798f921416c68ee'
+                echo 'here we are testing the code'
+            }
+        }
     }
 }
